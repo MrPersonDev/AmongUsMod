8989616d38610c17564ca7324284ba2b19c3b77b
MrPersonDev <simonashton37@gmail.com>
2022-08-20 14:52:53 -0700

    Add among us nugget to craft raw among us

--- src/main/java/mrperson/amongus/item/ItemRegistry.java
+++ src/main/java/mrperson/amongus/item/ItemRegistry.java
@@ -10,9 +10,11 @@ import net.minecraft.util.registry.Registry;
 public class ItemRegistry {
     public static final Item RAW_AMONG_US = new Item(new FabricItemSettings().group(AmongUs.ITEMGROUP));
     public static final Item AMONG_US_INGOT = new Item(new FabricItemSettings().group(AmongUs.ITEMGROUP));
+    public static final Item AMONG_US_NUGGET = new Item(new FabricItemSettings().group(AmongUs.ITEMGROUP));
 
     public static void init() {
         Registry.register(Registry.ITEM, new Identifier("amongus", "raw_among_us"), RAW_AMONG_US);
         Registry.register(Registry.ITEM, new Identifier("amongus", "among_us_ingot"), AMONG_US_INGOT);
+        Registry.register(Registry.ITEM, new Identifier("amongus", "among_us_nugget"), AMONG_US_NUGGET);
     }
 }
