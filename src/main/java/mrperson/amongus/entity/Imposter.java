83a3e947b17620334c9df562e969834f692482fe
MrPersonDev <simonashton37@gmail.com>
2022-08-23 19:35:26 -0700

    Imposter inflicts blindness when hitting player

--- src/main/java/mrperson/amongus/entity/Imposter.java
+++ src/main/java/mrperson/amongus/entity/Imposter.java
@@ -1,6 +1,7 @@
 package mrperson.amongus.entity;
 
 import mrperson.amongus.sound.SoundRegistry;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityPose;
 import net.minecraft.entity.EntityType;
@@ -8,7 +9,10 @@ import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.mob.ZombieEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
@@ -33,6 +37,17 @@ public class Imposter extends ZombieEntity {
         if (random.nextBoolean()) return false;
 
         return canMobSpawn(type, world, spawnReason, pos, random);
+    }
+
+    @Override
+    public boolean tryAttack(Entity target) {
+        boolean bl = super.tryAttack(target);
+
+        if (bl && target instanceof PlayerEntity) {
+            ((PlayerEntity)target).addStatusEffect(new StatusEffectInstance(StatusEffects.BLINDNESS, 200, 1));
+        }
+        
+        return bl;
     }
 
     protected boolean burnsInDaylight() {
